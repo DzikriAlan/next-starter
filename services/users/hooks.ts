d0302e2cc69a5cf84436baef01441d67e38eefce
dzikrirama <dzikripride@gmail.com>
2026-09-19 09:12:03 +0700

    chore: remove unused users service helpers

--- services/users/hooks.ts
+++
@@ -1,8 +0,0 @@
-// User related hooks
-export const useUser = () => {
-  // Logic will be added here
-  return {
-    user: null,
-    loading: false
-  }
-}
--- services/users/utils.ts
+++
@@ -1,5 +0,0 @@
-// User related utility functions
-export const formatUserName = (user: any) => {
-  if (!user) return ''
-  return `${user.firstName} ${user.lastName}`
-}
